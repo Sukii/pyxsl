23480c38cd1e6ea2b59a6d875f45bda328d60873
sukii <suki.venkat@gmail.com>
2023-10-19 11:46:30 +0530

    adding docx pusher

--- pydocx4xsl.py
+++ pydocx4xsl.py
@@ -7,8 +7,9 @@ from saxonche import *
 xsls = ["xsl/iden.xsl","xsl/text.xsl"]
 
 # getting docx input file
-file_name = sys.argv[1]
-doc_zip = ZipFile(file_name, 'r')
+docx_file = sys.argv[1]
+docx_out_file = docx_file.replace(".docx","_output.docx")
+doc_zip = ZipFile(docx_file, 'r')
 doc_xml_file = doc_zip.open("word/document.xml",)
 doc_xml  = io.TextIOWrapper(doc_xml_file, encoding='utf-8', newline='').read()
 
@@ -22,6 +23,20 @@ with PySaxonProcessor(license=False) as proc:
             document = proc.parse_xml(xml_text=out_xml)
             executable = xsltproc.compile_stylesheet(stylesheet_file=xsl)
             out_xml = executable.transform_to_string(xdm_node=document)
-            print("output{}: {}".format(i+1,out_xml))
+            #print("output{}: {}".format(i+1,out_xml))
     except PySaxonApiError as err:
         print('Error during function call', err)
+
+    # write output to docx file
+    # there is no simple command to replace a file in zip
+    # Iterate the input files
+    with ZipFile(docx_out_file, mode="w") as out_docx:
+        for inzipinfo in doc_zip.infolist():
+            # Read input file
+            with doc_zip.open(inzipinfo) as infile:
+                content = infile.read()
+                if inzipinfo.filename == "word/document.xml":
+                    # copy the modified content
+                    out_docx.writestr(inzipinfo.filename, out_xml)
+                else: # copy the content
+                    out_docx.writestr(inzipinfo.filename, content)
