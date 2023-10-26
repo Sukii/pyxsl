e07303f1859ffb068315ec9cfc2cbf2e10610120
sukii <suki.venkat@gmail.com>
2023-10-26 22:57:33 +0530

    adding text out test2.py

---
+++ test2.py
@@ -0,0 +1,25 @@
+import sys
+from pyxsl import pydocx
+from zipfile import ZipFile
+
+
+# applying sequence of xsl
+xsls = ["xsl/iden.xsl","xsl/text.xsl"]
+
+# getting docx input file
+
+docx_file = sys.argv[1]
+docx_output_path = docx_file.replace(".docx","_output.docx")
+
+
+docx = ZipFile(docx_file, "r")
+
+
+xml_paths = ["word/document.xml"]
+
+# apply sequence of xsl on docx to extract xml
+xml_outputs = []
+xml_outputs.append(pydocx.xslDocx(docx,xml_paths[0],xsls))
+
+# print xml
+print(xml_outputs[0])
